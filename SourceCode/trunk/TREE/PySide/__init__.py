2b2f209badf42506839a94a1cf8191aef09d5817
lix@battelle.org <lix@battelle.org@7425d52a-73b5-e5a0-ea96-ce7777dba0e7>
2013-04-15 15:16:15 +0000

    Adding dependencies

---
+++ SourceCode/trunk/TREE/PySide/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ['QtCore', 'QtGui', 'QtNetwork', 'QtOpenGL', 'QtSql', 'QtSvg', 'QtTest', 'QtWebKit', 'QtScript']
+__version__         = "1.1.0"
+__version_info__    = (1, 1, 0, "final", 1)
