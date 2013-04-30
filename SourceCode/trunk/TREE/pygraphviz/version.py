8db09b62a67b64ef0cbac914a5eaab51235ed0b7
nguyenl@battelle.org <nguyenl@battelle.org@7425d52a-73b5-e5a0-ea96-ce7777dba0e7>
2013-04-30 16:22:35 +0000

    pygraphviz compiled for windows

---
+++ SourceCode/trunk/TREE/pygraphviz/version.py
@@ -0,0 +1,11 @@
+"""
+Version information for PyGraphviz, created during installation.
+
+Do not add this file to the repository.
+
+"""
+
+__version__ = '1.1.dev1952'
+__revision__ = '1952'
+__date__ = 'Thu Apr 14 11:28:08 2011'
+
